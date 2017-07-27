fab72f82554051a17aa1359c80fbd21489ea03ea
Joshua Davis <me@jdav.is>
2017-07-27 17:16:24 -0400

    checking issues in formatting

--- themes/azure/templates/layout/05_page.php
+++ themes/azure/templates/layout/05_page.php
@@ -4,47 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-4">
-            <!-- Navigation -->
-            <?php
-            $rendertree = $tree;
-            $path = '';
-
-            if ($page['language'] !== '') {
-                $rendertree = $tree[$page['language']];
-                $path = $page['language'];
-            }
-
-            echo $this->get_navigation($rendertree, $path, isset($params['request']) ? $params['request'] : '', $base_page, $params['mode']);
-            ?>
-
-            <div class="Links">
-                <?php if (!empty($params['html']['links'])) {
-                ?>
-                    <hr/>
-                    <?php foreach ($params['html']['links'] as $name => $url) {
-                    ?>
-                        <a href="<?= $url ?>" target="_blank"><?= $name ?></a>
-                        <br />
-                    <?php
-                    } ?>
-                <?php
-                } ?>
-
-                <?php if (!empty($params['html']['twitter'])) {
-                ?>
-                    <hr/>
-                    <div class="Twitter">
-                        <?php foreach ($params['html']['twitter'] as $handle) {
-                        ?>
-                            <iframe allowtransparency="true" frameborder="0" scrolling="no" style="width:162px; height:20px;" src="https://platform.twitter.com/widgets/follow_button.html?screen_name=<?= $handle; ?>&amp;show_count=false"></iframe>
-                            <br />
-                            <br />
-                        <?php
-                        } ?>
-                    </div>
-                <?php
-                } ?>
-            </div>
+            Test
         </div>
         <div class="col-md-8">
             <?= $this->section('content'); ?>
