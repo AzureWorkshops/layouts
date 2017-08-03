424a344c82282d70cddb5a934e21a3395a80692e
Joshua Davis <me@jdav.is>
2017-08-03 16:09:24 -0400

    adding affix changes

--- themes/azure/templates/layout/05_page.php
+++ themes/azure/templates/layout/05_page.php
@@ -9,58 +9,60 @@
     </div>
     <div class="row">
         <div class="col-md-2 col-md-offset-1 hidden-print">
-            <!-- Navigation -->
-            <?php
-            $rendertree = $tree;
-            $path = '';
-
-            if ($page['language'] !== '') {
-                $rendertree = $tree[$page['language']];
-                $path = $page['language'];
-            }
-
-            echo $this->get_navigation($rendertree, $path, isset($params['request']) ? $params['request'] : '', $base_page, $params['mode']);
-            ?>
-
-            <div class="Links">
-                <?php if (!empty($params['html']['links'])) {
-                ?>
-                    <hr/>
-                    <?php foreach ($params['html']['links'] as $name => $url) {
-                    ?>
-                        <a href="<?= $url ?>" target="_blank"><?= $name ?></a>
-                        <br />
-                    <?php
-                    } ?>
+            <div data-spy="affix" data-offset-top="100">
+                <!-- Navigation -->
                 <?php
-                } ?>
+                $rendertree = $tree;
+                $path = '';
 
-                <div class="pdf-link">
-                    <hr />
-                    <a href="[pdf_link]"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> <span>Download PDF</span></a>
-                </div>
+                if ($page['language'] !== '') {
+                    $rendertree = $tree[$page['language']];
+                    $path = $page['language'];
+                }
 
-                <?php if (!empty($params['html']['twitter'])) {
+                echo $this->get_navigation($rendertree, $path, isset($params['request']) ? $params['request'] : '', $base_page, $params['mode']);
                 ?>
-                    <hr/>
-                    <div class="Twitter">
-                        <?php foreach ($params['html']['twitter'] as $handle) {
+
+                <div class="Links">
+                    <?php if (!empty($params['html']['links'])) {
+                    ?>
+                        <hr/>
+                        <?php foreach ($params['html']['links'] as $name => $url) {
                         ?>
-                            <iframe allowtransparency="true" frameborder="0" scrolling="no" style="width:162px; height:20px;" src="https://platform.twitter.com/widgets/follow_button.html?screen_name=<?= $handle; ?>&amp;show_count=false"></iframe>
-                            <br />
+                            <a href="<?= $url ?>" target="_blank"><?= $name ?></a>
                             <br />
                         <?php
                         } ?>
+                    <?php
+                    } ?>
+
+                    <div class="pdf-link">
+                        <hr />
+                        <a href="[pdf_link]"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> <span>Download PDF</span></a>
                     </div>
-                <?php
-                } ?>
+
+                    <?php if (!empty($params['html']['twitter'])) {
+                    ?>
+                        <hr/>
+                        <div class="Twitter">
+                            <?php foreach ($params['html']['twitter'] as $handle) {
+                            ?>
+                                <iframe allowtransparency="true" frameborder="0" scrolling="no" style="width:162px; height:20px;" src="https://platform.twitter.com/widgets/follow_button.html?screen_name=<?= $handle; ?>&amp;show_count=false"></iframe>
+                                <br />
+                                <br />
+                            <?php
+                            } ?>
+                        </div>
+                    <?php
+                    } ?>
+                </div>
             </div>
         </div>
         <div class="col-md-6">
             <?= $this->section('content'); ?>
         </div>
         <div class="col-md-2 hidden-print">
-            <div class="article-links">
+            <div class="article-links" data-spy="affix" data-offset-top="100">
                 [article_map]
             </div>
         </div>
