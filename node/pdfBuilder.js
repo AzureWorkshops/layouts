717f9e45a1fce494716ec9dc605ed2211c7b64b0
Joshua Davis <me@jdav.is>
2017-08-03 11:40:17 -0400

    fixing path

--- node/pdfBuilder.js
+++ node/pdfBuilder.js
@@ -83,7 +83,7 @@ function pdfBuilder() {
                 let file = files[i];
 
                 var rel = path.relative(file, __dirname + '/static/pdf') + '/' + docs.title + '.pdf';
-console.log(`file: ${file} path: ${path}`);
+console.log(`file: ${file} path: ${rel}`);
                 let options = {
                     patterns: [
                         {
