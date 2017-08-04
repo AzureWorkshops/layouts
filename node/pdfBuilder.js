c1cc3fe949ea5942bf713ec9486fe691e705594b
Joshua Davis <me@jdav.is>
2017-08-04 16:22:24 -0400

    fixed typo

--- node/pdfBuilder.js
+++ node/pdfBuilder.js
@@ -50,7 +50,7 @@ function pdfBuilder() {
                     }
 
                     let html = fs.readFileSync(file).toString()
-                        .replace(/(src|(?:[^a]) href)(="|=')((?!(https)|(http)|(\/\/))(\.*\/?)*(\w))([^'"]*)("|')/g, `$1$2file://${__dirname}/static/$9$9$10`)
+                        .replace(/(src|(?:[^a]) href)(="|=')((?!(https)|(http)|(\/\/))(\.*\/?)*(\w))([^'"]*)("|')/g, `$1$2file://${__dirname}/static/$8$9$10`)
                         .replace(/(src|href)(="|=')(\/\/)([^'"]*)("|')/g, '$1$2https://$4$5');
 
                     let fullPath = './tmp' + path.dirname(file).replace(__dirname + '/static', '') + '/' + path.basename(file, '.html') + '.pdf';
